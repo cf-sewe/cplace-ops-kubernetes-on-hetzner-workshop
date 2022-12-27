752a83a79ac0c2240ac0260498a9f08665fcbd49
Sebastian W <sebastian.weitzel@collaboration-factory.de>
2022-12-27 09:53:38 +0100

    Sort IPs to prevent recreate
    Signed-off-by: Sebastian W <sebastian.weitzel@collaboration-factory.de>

--- terraform/hcloud_controlplane.tf
+++ terraform/hcloud_controlplane.tf
@@ -40,47 +40,47 @@ resource "hcloud_firewall" "controlplane" {
     direction   = "in"
     protocol    = "tcp"
     port        = "443"
-    source_ips = concat(
+    source_ips = sort(concat(
       [format("%s/32", hcloud_server.jump.ipv4_address)],
       [for s in hcloud_server.controlplane : format("%s/32", s.ipv4_address)]
-    )
+    ))
   }
   rule {
     description = "Allow inbound private Kubernetes API traffic"
     direction   = "in"
     protocol    = "tcp"
     port        = "6443"
-    source_ips = concat(
+    source_ips = sort(concat(
       [
         format("%s/32", hcloud_server.jump.ipv4_address),
         format("%s/32", hcloud_load_balancer.controlplane.ipv4),
       ],
       [for s in hcloud_server.controlplane : format("%s/32", s.ipv4_address)]
-    )
+    ))
   }
   rule {
     description = "Allow inbound private Talos traffic"
     direction   = "in"
     protocol    = "tcp"
     port        = "50000-50001"
-    source_ips = concat(
+    source_ips = sort(concat(
       [format("%s/32", hcloud_server.jump.ipv4_address)],
       [for s in hcloud_server.controlplane : format("%s/32", s.ipv4_address)]
-    )
+    ))
   }
   rule {
     description = "Allow inbound private Kubernetes traffic (etcd server client API)"
     direction   = "in"
     protocol    = "tcp"
     port        = "2379-2380"
-    source_ips  = [for s in hcloud_server.controlplane : format("%s/32", s.ipv4_address)]
+    source_ips  = sort([for s in hcloud_server.controlplane : format("%s/32", s.ipv4_address)])
   }
   rule {
     description = "Allow inbound private Kubernetes traffic (Kubelet API)"
     direction   = "in"
     protocol    = "tcp"
     port        = "10250"
-    source_ips  = [for s in hcloud_server.controlplane : format("%s/32", s.ipv4_address)]
+    source_ips  = sort([for s in hcloud_server.controlplane : format("%s/32", s.ipv4_address)])
   }
   apply_to {
     label_selector = "type=controlplane"
